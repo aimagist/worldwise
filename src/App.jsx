90e031e3c93dfb30d04885cc0993699809b72ca0
Federico Michel <efemichel@gmail.com>
2023-07-27 12:15:43 -0300

    finish building initial pages layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
           <Route path="app" element={<AppLayout />} />
         </Routes>
